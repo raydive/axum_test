79427caa732bdc89d7fa585e1938920ddd01d839
raydive <raywassmas@gmail.com>
2024-02-17 15:58:31 +0900

    tracing crateとotel関連crateをアップデーtl

--- src/otel.rs
+++ src/otel.rs
@@ -1,27 +1,26 @@
 use hyper::Request;
-use opentelemetry::{
-    global,
-    sdk::{export::trace::stdout, trace::Tracer},
-};
+use opentelemetry::{global, trace::TracerProvider as _};
+
 use opentelemetry_http::HeaderExtractor;
+use opentelemetry_sdk::trace::{Tracer, TracerProvider};
 use tracing::{subscriber, Span};
-use tracing_opentelemetry::{OpenTelemetryLayer, OpenTelemetrySpanExt};
+use tracing_opentelemetry::OpenTelemetrySpanExt;
 use tracing_subscriber::layer::SubscriberExt;
 
 pub fn init_tracer() -> Tracer {
-    let tracer = stdout::new_pipeline().install_simple();
-
-    tracer
+    let provider = TracerProvider::builder()
+        .with_simple_exporter(opentelemetry_stdout::SpanExporter::default())
+        .build();
+    provider.tracer("otel/sample")
 }
 
 pub fn setup_subscriber(tracer: Tracer) {
-    opentelemetry::global::set_text_map_propagator(
-        opentelemetry::sdk::propagation::TraceContextPropagator::new(),
-    );
+    global::set_text_map_propagator(opentelemetry_sdk::propagation::TraceContextPropagator::new());
 
-    let layer = OpenTelemetryLayer::new(tracer).with_location(true);
+    let layer = tracing_opentelemetry::layer().with_tracer(tracer);
+    let log = tracing_subscriber::fmt::layer();
 
-    let subscriber = tracing_subscriber::Registry::default().with(layer);
+    let subscriber = tracing_subscriber::Registry::default().with(layer).with(log);
     subscriber::set_global_default(subscriber).expect("setting tracing default failed");
 }
 
@@ -30,7 +29,7 @@ pub fn make_span<B>(req: &Request<B>) -> Span {
         propagator.extract(&HeaderExtractor(req.headers()))
     });
     let span = tracing::info_span!(
-        "request",
+        "a request",
         method = %req.method(),
         path = %req.uri().path(),
         version = ?req.version(),
@@ -39,6 +38,7 @@ pub fn make_span<B>(req: &Request<B>) -> Span {
         query = ?req.uri().query(),
         request_id = tracing::field::Empty,
     );
+    span.set_attribute("att", "you show me!");
 
     span.set_parent(ctx);
     span
@@ -50,7 +50,7 @@ pub fn on_request<B>(req: &Request<B>, span: &Span) {
         &tracing::field::debug(
             req.headers()
                 .get("x-request-id")
-                .unwrap_or(&"none".parse().unwrap()),
+                .unwrap_or(&"who are you?".parse().unwrap()),
         ),
     );
 
